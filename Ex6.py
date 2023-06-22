46d7d3ce0385c2bd9e8c3560b8b92fc597c31070
v.lobyntsev <vikandlob@gmail.com>
2023-06-22 07:07:15 +0300

    ex6_2

--- Ex6.py
+++ Ex6.py
@@ -2,9 +2,7 @@ import requests
 
 url = "https://playground.learnqa.ru/api/long_redirect"
 response = requests.get(url, allow_redirects=True)
-first_response = response.history[0]
-second_response = response.history[1]
-hz_response = response
-print(first_response.url)
-print(second_response.url)
-print(hz_response.url)
+redirects = response.history
+last_url = response.url
+print(f"Количество редиректов: {len(redirects)}")
+print(f"Итоговый URL: {last_url}")
