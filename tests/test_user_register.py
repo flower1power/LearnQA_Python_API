4c8f08628a216031798a7e9e1b4e4db0d68bf31b
v.lobyntsev <vikandlob@gmail.com>
2023-07-10 20:01:38 +0300

    Ex15 added test_create_user_with_invalid_email / test_create_user_without_field / test_create_user_with_short_name / test_create_user_with_long_name

---
+++ tests/test_user_register.py
@@ -0,0 +1,118 @@
+import pytest
+import requests
+from parameterized import parameterized
+
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+# from datetime import datetime
+
+class TestUserRegister(BaseCase):
+    # def setup(self):
+    #     base_part = "learnqa"
+    #     domain = "example.com"
+    #     random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+    #     self.email = f"{base_part}{random_part}@{domain}"
+
+
+
+    def test_craete_user_successfully(self):
+
+        data = self.prepare_registration_data()
+        # data = {
+        #     'password': '123',
+        #     'username': 'learnqa',
+        #     'firstName': 'learnqa',
+        #     'lastName': 'learnqa',
+        #     'email': self.email
+        # }
+
+        # response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
+
+
+        # assert response.status_code == 200, f"Unexpected status code {response.status_code}"
+        # print(response.content)
+        Assertions.asert_code_status(response, expected_status_code=200)
+        Assertions.assert_json_has_key(response, "id")
+
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data = self.prepare_registration_data(email)
+        # data = {
+        #     'password': '123',
+        #     'username': 'learnqa',
+        #     'firstName': 'learnqa',
+        #     'lastName': 'learnqa',
+        #     'email': email
+        # }
+        # response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
+        
+        # print(response.status_code)
+        # print(response.content)
+
+        # assert response.status_code == 400, f"Unexpected status code {response.status_code}"
+        Assertions.asert_code_status(response, expected_status_code=400)
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected content {response.content}"
+
+    def test_create_user_with_invalid_email(self):
+        email = 'invalid_email'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.asert_code_status(response, expected_status_code=400)
+        assert response.content.decode("utf-8") == "Invalid email format"
+
+
+    @pytest.mark.parametrize("field, data", [
+        ("password",
+         {"username": "learnqa", "firstName": "learnqa", "lastName": "learnqa", "email": "test@example.com"}),
+        ("username", {"password": "123", "firstName": "learnqa", "lastName": "learnqa", "email": "test@example.com"}),
+        ("firstName", {"password": "123", "username": "learnqa", "lastName": "learnqa", "email": "test@example.com"}),
+        ("lastName", {"password": "123", "username": "learnqa", "firstName": "learnqa", "email": "test@example.com"}),
+        ("email", {"password": "123", "username": "learnqa", "firstName": "learnqa", "lastName": "learnqa"})
+    ])
+    def test_create_user_without_field(self, field, data):
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.asert_code_status(response, expected_status_code=400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {field}"
+
+
+
+    def test_create_user_with_short_name(self):
+        name = 'a'
+        data = {
+            'password': '123',
+            'username': name,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+        }
+
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.asert_code_status(response, expected_status_code=400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too short"
+
+    def test_create_user_with_long_name(self):
+        name = 'a'*251
+        data = {
+            'password': '123',
+            'username': name,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+        }
+
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.asert_code_status(response, expected_status_code=400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too long"
+
+
+
